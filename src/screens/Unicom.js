c01b5496b53d881e59c7100661cc59e3a0c14f45
apple <apple@apples-MacBook-Pro.local>
2025-02-10 11:28:07 +0530

    pre final

--- src/screens/Unicom.js
+++ src/screens/Unicom.js
@@ -25,7 +25,7 @@ const Unicom = () => {
         beforeDraw: (chart) => {
             const { ctx, chartArea } = chart;
             const percentage = Math.round(progress); // Get the current progress percentage
-            const fontSize = 60;
+            const fontSize = 40;
 
             // Set text color and style
             ctx.font = `${fontSize}px Raleway`;
@@ -44,22 +44,210 @@ const Unicom = () => {
 
     Chart.register(percentagePlugin);
 
-    const data = {
-        labels: [],
-        datasets: [
-            {
-                data: [progress, 100 - progress],
-                backgroundColor: (context) => {
-                    const { ctx } = context.chart; // Get the context of the chart
-                    return [
-                        "#C3DBFF", // Background color for the remaining part
-                        createGradient(ctx), // Apply gradient for the progress part
-                    ];
-                },
-                borderWidth: 0, // Optional: Remove border between segments
+    const leverdata = [
+        {
+            name: "Physiological Health",
+            desc: "assesses if people take up an active role in enhancing their physical well-being for optimal performance and productivity",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
             },
-        ],
-    };
+        },
+        {
+            name: "Social Health",
+            desc: "assesses if people take up an active role in enhancing their physical well-being for optimal performance and productivity",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [81, 100 - 81],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+        {
+            name: "Career Health",
+            desc: "evaluates professional nurturing within the organization and provides overall security, ensuring not just job stability but a promising career",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+        {
+            name: "Financial Health",
+            desc: "examines employees' financial awareness of the status quo and efforts towards economic stability",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+        {
+            name: "Culture Health",
+            desc: "explores how the practices and values followed are unique, bragworthy, and effective in crafting a great experience",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+        {
+            name: "Mental Health",
+            desc: "examines if the workplace fosters emotional safety, ease of expression, and satisfaction for robust mental health",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+        {
+            name: "Intellectual Health",
+            desc: "indicates the level of synergy in the area to become a dynamic learning and innovative environment",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+        {
+            name: "Tech & Digital Health",
+            desc: "indicates how well the organization has adopted Data & Technology to be a faster, better and a smarter workplace",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+
+        {
+            name: "Business Health",
+            desc: "understands the perceived organizational value through brand resonance, leadership and impact creation",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+        {
+            name: "Spiritual Health",
+            desc: "assesses the alignment of individual purpose and their inclination to a larger societal mission",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [77, 100 - 77],
+                        backgroundColor: (context) => {
+                            const { ctx } = context.chart;
+                            return [
+                                "#C3DBFF",
+                                createGradient(ctx),
+                            ];
+                        },
+                        borderWidth: 0,
+                    },
+                ],
+            },
+        },
+
+    ]
 
     const options = {
         responsive: true,
@@ -117,43 +305,62 @@ const Unicom = () => {
             <div style={{ backgroundImage: `url(${bgImage})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", height: '100%' }} className="z-10 w-[60%] -mt-[10%] flex justify-center items-center">
                 {/* <img alt="path_line" src={require("../assets/path_line.png")} /> */}
                 <div className="grid grid-cols-2 gap-8 w-[100%] mt-[10%] place-items-center z-10 rounded-3xl ">
-                    <div className="bg-white rounded-xl w-96 h-90 flex-col p-4">
-                        <div className="rounded-full border border-[#0064FF] w-32 h-32 -ml-12 -mt-12 bg-white flex justify-center items-center shadow-lg shadow-white">
-                            <img alt="physiological_health_icon" className="w-24" src={require("../assets/physiological-health-icon.png")} />
-                        </div>
-                        <div className="flex w-full justify-center items-center flex-col -mt-[10%]">
-                            <Doughnut className="w-[45%]" data={data} options={options} />
-                        </div>
-                        <p className="font-bold text-2xl text-center">Physiological Health</p>
-                        <p>assesses if people take up an active role in enhancing their physical well-being for optimal performance and productivity</p>
+                    {leverdata.map((lever) => (
+                        <div className="bg-white rounded-xl w-96 h-80 flex-col p-4">
+                            <div className="rounded-full border border-[#0064FF] w-32 h-32 -ml-12 -mt-12 bg-white flex justify-center items-center shadow-lg shadow-white">
+                                <img alt="physiological_health_icon" className="w-24" src={require("../assets/physiological-health-icon.png")} />
+                            </div>
+                            <div className="flex justify-center items-center flex-col -mt-[20%]">
+                                <div className="flex w-40 h-40 justify-center items-center flex-col ">
+                                    <Doughnut className="" data={lever.data} options={options} />
+                                </div>
+                                <p className="font-bold text-2xl text-center mt-3">{lever.name}</p>
+                                <p>{lever.desc}</p>
 
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+            <div className="flex flex-row justify-evenly items-center mt-5 z-10">
+                <img alt="people1" className="w-[40%]" src={require("../assets/people1.png")} />
+                <div>
+                    <p className="text-2xl text-white text-center">Do you still measure engagement, satisfaction & culture? <span className="text-[#FFE70B] font-bold">Go beyond engagement.</span></p>
+                    <p className="text-2xl text-white font-bold text-center">Take the Thriving Approach.</p>
+                </div>
+                <img alt="people2" className="w-[40%]" src={require("../assets/people2.png")} />
+            </div>
+            <div className="w-full -mt-[10%]">
+                <img alt="people-line" src={require("../assets/people-line.png")} />
+            </div>
+            <div className="w-full flex justify-center items-center flex-col">
+                <div className="bg-gradient-to-t from-[#FFE501] to-[#FEF073] p-3 rounded-lg font-semibold cursor-pointer">
+                    <p>Visit Our Website</p>
+                </div>
+                <p className="text-white font-semibold mt-3 text-xl">Make your organizing a Thriving workplace</p>
+            </div>
+            <div className="w-full flex justify-center items-center flex-col mt-11">
+                <div className="w-[70%] bg-white rounded-xl flex justify-around items-center pt-5 pb-5 mb-10">
+                    <div>
+                        <div className="flex  items-center flex-row">
+                            <p className="text-[#0042A6] font-bold text-lg">About Aspire</p>
+                            <img alt="arrow-mark" className="w-[20px] h-[20px] ml-2" src={require("../assets/arrow.png")} />
+                        </div>
+                        <p className="text-lg">Contact <span className="underline">krishna.prasad@spigroup.in</span></p>
+                        <p className="text-lg">Email <span className="underline">+91 94458 95581</span></p>
+                        <div className="w-[70%] mt-8">
+                            <p className="text-[#0042A6] font-semibold text-3xl">Become the Employer of Choice.</p>
+                        </div>
+                        <div className="mt-10 flex justify-center items-center flex-row">
+                            <input placeholder="| Enter your business email" className="rounded-l-lg border border-r-transparent border-[#0042A6] p-2" />
+                            <div className="bg-gradient-to-t from-[#FFE501] to-[#FEF073] p-3 rounded-lg font-semibold cursor-pointer">
+                                <p>Visit Our Website</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
-                    </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
-                    </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
-                    </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
-                    </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
-                    </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
-                    </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
-                    </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
-                    </div>
-                    <div className="bg-white rounded-xl w-80 h-96">
-                        <p>Physiological Health</p>
+                    <div className="w-80 h-auto">
+                        <img alt="people" src={require("../assets/people.png")} />
                     </div>
                 </div>
             </div>
